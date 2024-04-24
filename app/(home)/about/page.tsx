2052753c2a939eea697bce8a5420a7669fefbcc9
Catherine Maher <catymaher4@gmail.com>
2024-04-24 16:27:56 +0200

    About design

---
+++ app/(home)/about/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="flex justify-center items-center py-40 flex-col ">
+      <img src="/images/LOGO.png" alt="Rank.Dev" style={{width:"20%"}}/>
+      <p className="container text-xl py-10">
+      In today’s fast-paced world, it can be challenging to meet new people.That’s why we created Chitchat.gg
+      - to make it easier for people to connect with each other. Our platform allows you to chat with people
+      from all over the world, so you can expand your social circle and make meaningful connections. In
+      addition, our platform offers loads of features and tools to make your chatting experience more
+      enjoyable and interactive. Join us today and start exploring a whole new world of socializing!
+      </p>
+    </div>
+  )
+}
