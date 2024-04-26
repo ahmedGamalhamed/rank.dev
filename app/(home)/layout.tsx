2f9365ed587f6387dcfbe8c0b74483debb68b8e2
Mohamed Mounir <116511682+mohamedmounir2425@users.noreply.github.com>
2024-04-26 23:00:51 +0300

    create navbar

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,9 +1,11 @@
-export default function Layout({ children }: Readonly<{
-    children: React.ReactNode;
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <>
-            <main>{children}</main>
-        </>
-    )
+  return (
+    <>
+      <main>{children}</main>
+    </>
+  );
 }
