da4b75fe884bdb6e1e039f19a74cfb0b38021b78
ahmedGamalHamed <ahmed.gamalhamed5@gmail.com>
2024-04-29 02:06:12 +0300

    socket url log update

--- next/app/(socket)/socket.ts
+++ next/app/(socket)/socket.ts
@@ -1,5 +1,7 @@
 import { io } from 'socket.io-client';
 
+console.log('SOCKET URL: ', process.env.SOCKET_SERVER_URL);
+
 export const socket = io(process.env.SOCKET_SERVER_URL!, {
   autoConnect: false,
 });
