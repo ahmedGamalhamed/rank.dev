3d755099dcafb4bba578f7d1c4e2c139f104b26f
ahmedGamalHamed <ahmed.gamalhamed5@gmail.com>
2024-04-29 02:09:44 +0300

    hardcoded socket server

--- next/app/(socket)/socket.ts
+++ next/app/(socket)/socket.ts
@@ -2,6 +2,6 @@ import { io } from 'socket.io-client';
 
 console.log('SOCKET URL: ', process.env.SOCKET_SERVER_URL);
 
-export const socket = io(process.env.SOCKET_SERVER_URL!, {
+export const socket = io('https://rank-dev.onrender.com', {
   autoConnect: false,
 });
