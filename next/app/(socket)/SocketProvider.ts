ac9d37de605536e66018aa453f21885725f381d7
ahmedGamalHamed <ahmed.gamalhamed5@gmail.com>
2024-04-29 02:01:27 +0300

    socket-server-url update

--- next/app/(socket)/SocketProvider.ts
+++ next/app/(socket)/SocketProvider.ts
@@ -1,17 +1,15 @@
-"use client";
+'use client';
 
-import React, { useEffect } from "react";
-import { socket } from "./socket";
+import React, { useEffect } from 'react';
+import { socket } from './socket';
 
 export default function SocketProvider() {
   useEffect(() => {
-    console.log("Connecting");
     if (socket.disconnected) {
       socket.connect();
     }
 
     return () => {
-      console.log("Disconnecting");
       socket.disconnect();
     };
   }, []);
--- next/app/(socket)/socket.ts
+++ next/app/(socket)/socket.ts
@@ -1,5 +1,5 @@
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
 
-export const socket = io(process.env.SOCKET_SERVER_URL || "http://localhost:4000", {
+export const socket = io(process.env.SOCKET_SERVER_URL!, {
   autoConnect: false,
 });
