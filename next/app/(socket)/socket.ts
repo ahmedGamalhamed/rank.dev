64b793441eba5f49557cf97255059432bba9f331
ahmedGamalhamed <ahmed.gamalhamed5@gmail.com>
2024-04-29 21:15:27 +0300

    socket url server

--- next/app/(socket)/socket.ts
+++ next/app/(socket)/socket.ts
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-const socketURL = 'http://localhost:4000';
+// const socketURL = 'http://localhost:4000';
+const socketURL = process.env.SOCKET_SERVER_URL!;
 console.log('SOCKET URL: ', socketURL);
 
 export const socket = io(socketURL, {
